ce9f92635295d41a277c769664b4a4748c71035f
terminator4088 <43856573+terminator4088@users.noreply.github.com>
2020-05-23 14:17:12 +0200

    Update net_io.c

--- net_io.c
+++ net_io.c
@@ -447,7 +447,8 @@ static void writeBeastMessage(struct net_writer *writer, uint64_t timestamp, dou
     *p++ = (char)sig;
 	*/
     double sig = log10(signalLevel);
-    int help = abs(int(round(sig*1000000)));
+	int help2 = (int)round(sig*1000000)
+    int help = abs(help2);
     *p++ = (j = help >> 24);
     *p++ = (j = help >> 16);
     *p++ = (j = help >> 8);
